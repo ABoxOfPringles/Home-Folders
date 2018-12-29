e99520caa06fdbab8ddc3d95ec51e2fb25504d07
ABoxOfPringles <46105595+ABoxOfPringles@users.noreply.github.com>
2018-12-29 01:41:24 -0800

    Add files via upload
    From Esperanto to German

---
+++ DE-EO.sh
@@ -0,0 +1,9 @@
+# Don't forget to chmod +x first!
+echo "Loading..."
+cd ~/Dokumentoj && mv -v * ~/Dokumente/
+cd ~/Elŝutoj && mv -v * ~/Downloads/
+cd ~/Labortablo && mv -v * ~/Schreibtisch/
+cd ~/Videoj && mv -v * ~/Videos/
+cd ~/Bildoj && mv -v * ~/Bilder/
+echo "Done!"
+
